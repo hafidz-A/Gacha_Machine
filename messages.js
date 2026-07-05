873c3957f404ca0c5b3337cd7b03e4d80748240b
Hafidz_A <73853883+hafidz-A@users.noreply.github.com>
2026-07-05 20:07:27 +0700

    Update messages.js

--- messages.js
+++ messages.js
@@ -63,13 +63,15 @@ export const pastMessages = [
   "apit aslinya ga jago gombal, tapi kalo sayang dedey aku jagonyaa 😙😙😙😙😙😙😙😙😙😙😙",
   "sebagai orang yang lumayan jago matematika 😎, ada 1 rumus yang kamu tanyain aja deh ke aku by wa yaaa? lov yuuu...",
   "sekarang kalau dedey mau peluk apit ada bayarannya... harus peluk apit balikk 😤😤😤😤😤",
-  "tadi dedey bawa2 surat keputusan ke bapa jahat... aku jadi kepikiran kalo dedey juga perlu deh bawa ke apit, keputusan mencitaimu selalu🥰💗"
-];
-
-export const messages = [
+  "tadi dedey bawa2 surat keputusan ke bapa jahat... aku jadi kepikiran kalo dedey juga perlu deh bawa ke apit, keputusan mencitaimu selalu🥰💗",
   "kalau bayar biasanya aku make qris, tapi kalau ada kamu aku maunya kiss 🥰💗",
   "i love you baby💕💙",
   "besok mau kiss rasa apa? straberry? mangga? durian? akusih mau original aja, kiss (dedey)"
+];
+
+export const messages = [
+  "aku izin serius yah sayang, izin serius mencintai dedey sampee lama lama lama lamanyaaa....🥰🥰🥰🥰🥰🥰🥰🥰",
+  "setelah sebulanan pacaran sama kamu, yang aku paling rasain, kamu udah jadi motivasi aku yang gapernah ilang👉👈"
 ];
 
 export const openedMessages = new Set();
