9da5e8d5adfd06e8e6b050fea1dc7372bb629ef0
Hafidz_A <73853883+hafidz-A@users.noreply.github.com>
2026-08-02 21:21:10 +0700

    Update messages.js

--- messages.js
+++ messages.js
@@ -112,12 +112,16 @@ export const pastMessages = [
   "haii im still waiting for our 'SAH' day...🥹🥹",
   "kamu suka pentol ga sih? kalo apitt lebih suka kecantool dedeyy😝😝",
   "besok hari apaa? besok hari ketemu dedey lagi ke 108247019280519284 kalinya yey, izin kiss 1kalisatujuta 😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘",
-  "aku make kacamata mau gamake, liat dedey tetep sayang banget gaketolong🥹🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰"
+  "aku make kacamata mau gamake, liat dedey tetep sayang banget gaketolong🥹🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰",
+  "how lucky i am to have something that makes saying goodbye so hard, like goodbye dedey🥺🥺🥺🥺🥺🥺🥺❤️",
+  "dedeyku sayaang, hari ini aku tau dedey cape banget, sedih jugaa, tapi apit yakin dedeyy kuattt💪. Semoga Allah kasih gantinya buat besok2 dedey biar lebih baik drpd hari ini"
+
 ];
 
 export const messages = [
-  "how lucky i am to have something that makes saying goodbye so hard, like goodbye dedey🥺🥺🥺🥺🥺🥺🥺❤️",
-  "dedeyku sayaang, hari ini aku tau dedey cape banget, sedih jugaa, tapi apit yakin dedeyy kuattt💪. Semoga Allah kasih gantinya buat besok2 dedey biar lebih baik drpd hari ini"
+  "dedeyy sayaang, pelatihan minggu ini yang semangaat yaaa💕💕 apitt tunggu jadi advokat kesayangan apiitt😘",
+  "sayangkuu semoga tempat tinggal barunya nyaman, bikin dedey betah dan gabanyak ngeluh, jadi lebih deket lagi sama tan... love youu babyyy💕🥰",
+  "apittt seneng bisa nemenin dedeyy teruss sampe sekarang❤️"
 ];
 
 export const openedMessages = new Set();
