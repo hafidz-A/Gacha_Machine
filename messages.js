6bdcbde7428e0fb507f7e7869c1c4170fef80d78
Hafidz_A <73853883+hafidz-A@users.noreply.github.com>
2026-08-19 08:45:29 +0700

    Update messages.js

--- messages.js
+++ messages.js
@@ -148,7 +148,7 @@ export const pastMessages = [
 export const messages = [
   "baru tau aku singkatan NPWP tuh ternyata Need Peluk With Pukpuk😖❤️",
   "aku kangen nyetir ditemenin dedey:(... apit mau nyetirin dedey banyaaak2 soalnya dedey keliatan tenang banget kalo apit setirin😘",
-  "aku gatau emang kebetulan atau gimana, TA aku yang 1 tahun sama tece seakan2 ngebuat aku tuh gampang banget nerima kamu sayang💙💜💕💗 mungkin itu sebabnya dulu aku belum ketemu sama dedey, disuruh belajar dulu bergaul sama temennya dedey, kaya inikah jalannya?",
+  "TA aku yang 1 tahun sama tece seakan2 ngebuat aku tuh gampang banget nerima kamu sayang💙💜💕💗 mungkin itu sebabnya dulu aku belum ketemu sama dedey, disuruh belajar dulu bergaul sama temennya dedey, aku selalu mikir kaya inikah jalannya?",
   "apitt seneng banget dedey ngedukung usaha2 apit... tapi kadang apit suka overstimulate dan akhirnya terlalu mikirin usahaku dan nyampingin yang lainnya, maaf ya aku mau belajar buat bisa ngontrol diriku😔😔😘"
 ];
 
